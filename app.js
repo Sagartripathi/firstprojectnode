f4257afd88558d6e9241349ddf55c659548c7e2c
Sagartripathi <tripathisagar1997@gmail.com>
2023-09-27 14:28:50 +0545

    after the nodemon app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const app = require("express")()
+const app = require("express")();
 
-// alternative ways 
+// alternative ways
 
-app.get("/",(req,res)=>{
-    res.send({
-        message:"It home page....."
-            })
-        })
-        app.get("/sagar/",(req,res)=>{
-            res.send({
-                message:"sagar"
-                    })
-                })
-app.listen(4100,(req, res)=>{
-    console.log(" its running on port 4100")
-})
-                   
+//command too start nodemon aap.js | command to rum in terminal npm run(it help to save code automatically )
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "It home page.....",
+  });
+});
+app.get("/sagar/", (req, res) => {
+  res.send({
+    message: "sagar",
+  });
+});
+app.listen(4100, (req, res) => {
+  console.log(" its running on port 4100");
+});
